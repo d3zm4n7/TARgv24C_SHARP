bb32e7719ac588c3f806801b1785838a40d82209
opilane <opilane@HP-CZC2349HTB>
2025-04-23 15:50:46 +0300

    git update

--- 5. Kollektsioonid/MainClass_5osa.cs
+++ 5. Kollektsioonid/MainClass_5osa.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TARgv24C_SHARP._5._Kollektsioonid
 {
@@ -28,6 +29,76 @@ namespace TARgv24C_SHARP._5._Kollektsioonid
 
             foreach (string nimi in nimed)
                 Console.WriteLine(nimi);
+
+            Console.ReadLine();
+
+
+
+            Tuple<float, char> route = new Tuple<float, char>(2.5f, 'N');
+            Console.WriteLine($"Vahemaa: {route.Item1}, Suund: {route.Item2}");
+
+            Console.ReadLine();
+
+            List<Person> people = new List<Person>();
+            people.Add(new Person() { Name = "Kadi" });
+            people.Add(new Person() { Name = "Mirje" });
+
+            foreach (Person p in people)
+                Console.WriteLine(p.Name);
+
+            Console.ReadLine();
+
+
+
+            LinkedList<int> loetelu = new LinkedList<int>();
+            loetelu.AddLast(5);
+            loetelu.AddLast(3);
+            loetelu.AddFirst(0);
+
+            foreach (int arv in loetelu)
+                Console.WriteLine(arv);
+
+            loetelu.RemoveFirst();
+            loetelu.RemoveLast();
+            loetelu.AddLast(555);
+            loetelu.Remove(555);
+
+            Console.ReadLine();
+
+
+
+            Dictionary<int, string> riigid = new Dictionary<int, string>();
+            riigid.Add(1, "Hiina");
+            riigid.Add(2, "Eesti");
+            riigid.Add(3, "Itaalia");
+
+            foreach (var paar in riigid)
+                Console.WriteLine($"{paar.Key} - {paar.Value}");
+
+            string pealinn = riigid[2];
+            riigid[2] = "Eestimaa";
+            riigid.Remove(3);
+
+            Console.ReadLine();
+
+
+            Dictionary<char, Person> inimesed = new Dictionary<char, Person>();
+            inimesed.Add('k', new Person() { Name = "Kadi" });
+            inimesed.Add('m', new Person() { Name = "Mait" });
+
+            foreach (var entry in inimesed)
+                Console.WriteLine($"{entry.Key} - {entry.Value.Name}");
+
+            Console.ReadLine();
+
+
+
         }
+
+    class Person
+    {
+        public string Name { get; set; }
     }
+        
+    }   
 }
